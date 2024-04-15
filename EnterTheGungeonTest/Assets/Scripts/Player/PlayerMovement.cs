2fd4e0682d5c84ce378b3c0afb70b391a157b160
FMarklew <fordmarklew123@gmail.com>
2024-04-15 11:55:52 -0400

    Initial scene, character movement, tilemaps and props imported, package with new unity input system imported

---
+++ EnterTheGungeonTest/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private InputActionReference movementInputAction;
+
+    [SerializeField] private Rigidbody2D rb;
+    void Update()
+    {
+        Vector2 moveDir = movementInputAction.action.ReadValue<Vector2>().normalized;
+
+        moveDir *= moveSpeed * Time.deltaTime;
+        //transform.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
+        rb.MovePosition(transform.position + new Vector3(moveDir.x, moveDir.y, 0));
+    }
+}
